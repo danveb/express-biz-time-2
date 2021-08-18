708670fff9f1529cc5c16b6aa3b1e486ca1e4c61
Daniel Bae <danibae91@icloud.com>
2021-08-17 22:58:28 -0700

    require slugify

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ExpressError = require('../expressError')
 const router = express.Router() 
 const db = require('../db')
+const slugify = require('slugify') 
 
 // GET /companies 
 // SQL: SELECT * FROM companies; 
@@ -55,6 +56,8 @@ router.post('/', async (req, res, next) => {
 // PUT /companies/:code
 // Note: The difference between PUT and PATCH is that PUT must update all fields to make it idempotent. This fancy word basically means that you must always get the same result no matter how many times it is executed. This is important in network traffic, because if you’re in doubt whether your request has been lost during transmission, you can just send it again without worrying about messing up the resource’s data.
 // SQL: UPDATE companies SET name=$1 description=$2 WHERE code=$3 RETURNING * 
+
+// Add slugify 
 router.put('/:code', async (req, res, next) => {
     try {
         // extract code from req.params
@@ -63,7 +66,7 @@ router.put('/:code', async (req, res, next) => {
         const { name, description } = req.body 
         // await db.query (SQL); return *
         // parameterize ($1, $2, $3) to prevent SQL Injection 
-        const results = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description`, [name, description, code])
+        const results = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description`, [name, description, slugify(name, {lower:true})])
         return res.send({company: results.rows[0]})
     } catch(err) {
         return next(err) 
